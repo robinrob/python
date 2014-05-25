fa78d664fe3808f658c578d21e5a9d2a183304ef
Robin Smith <robinsmith@Neptune-6.local>
2014-05-25 15:59:09 -0700

    Auto-update

--- projects/csv/csv.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from csv_generator import CSVGenerator
-from csv_writer import CSVWriter
-from csv_reader import CSVReader
-
-data = CSVReader().as_dict(CSVWriter().write(CSVGenerator().generate(10, 10), 'file.txt').name)
-
-print(str(data))
