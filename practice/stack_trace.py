c4759600538a76a9a7bdfc39223fb22aa3ceba8a
Robin Smith <robin.smith@cloudreach.co.uk>
2014-08-12 21:03:19 -0700

    Auto-update

--- practice/stack_trace.py
+++ practice/stack_trace.py
@@ -6,7 +6,19 @@ from termcolor import colored
 
 #print colored(dir(traceback), 'green')
 
-output = dir(traceback)
-print colored(output, 'green')
+# output = dir(traceback)
+# print colored(output, 'green')
 
-traceback.print_stack()
+def do_it():
+    try:
+        robin
+    except NameError as e:
+        # traceback.print_stack()
+        # traceback.print_exc()
+        # traceback.print_exception(NameError, e, traceback)
+        trace = traceback.format_exc()
+        print "trace: " + trace
+        pass
+        
+        
+do_it()
